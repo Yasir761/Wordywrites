2256e27acb38e81391ce911a8b73658d52eb6155
Yasir761 <myasiransari16@gmail.com>
2025-08-15 17:57:34 +0530

    remove the integration of google and X for now

--- src/app/api/integrations/google/types.ts
+++
@@ -1,9 +0,0 @@
-
-// export interface GoogleTokenData {
-//   access_token: string;
-//   refresh_token?: string;
-//   expiry_date?: number;
-//   scope: string;
-//   token_type: string;
-//   id_token?: string;
-// }
