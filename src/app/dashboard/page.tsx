f95636cd860906a0b237598bb2e6fa000fb409a6
Yasir761 <myasiransari16@gmail.com>
2025-11-22 14:25:15 +0530

    first commit for frontend revamp

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,6 +7,8 @@ import AIAgentsPanel from "./components/AiAgents"
 export default function DashboardPage() {
   return (
     <div className="space-y-12 px-4 xl:px-6 pb-12">
+
+      {/* revamp the dashboard layout and whole structure  */}
       <OverviewCards />
       <UsageChart />
       <RecentBlogs />
