e916ae2bea6014bb415f6f9f9e87ff9b0ef35993
Yasir761 <myasiransari16@gmail.com>
2026-01-24 21:12:50 +0530

    update hook comment

--- src/hooks/useUserstats.ts
+++ src/hooks/useUserstats.ts
@@ -2,6 +2,6 @@ import useSWR from "swr";
 
 export function useUserStats() {
   return useSWR("/api/user/stats", {
-    refreshInterval: 60000 // 1 minute
+    refreshInterval: 60000 // Refresh every 60 seconds
   });
 }
